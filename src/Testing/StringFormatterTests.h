d1a22fe5c6e93115014b76564f236eb21d8daeaa
Jeff Haynes <feedyurhed@gmail.com>
2024-12-27 14:38:17 -0500

    bigger test

--- src/Testing/StringFormatterTests.h
+++ src/Testing/StringFormatterTests.h
@@ -44,6 +44,14 @@ namespace StringFormatterTests
         return formatted == String("Hello, 1.0 2.00 3.000!");
     }(), "Format method failed");
 
+    // lots of arguments
+    static_assert([]{
+        Array<char, 100> buffer;
+        auto debug = StringFormatter::Format(buffer, "%6d %d %d \t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.3f\t%.3f\t%.3f\r\n", 
+            123456, 1, 2, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f);
+        return debug == String("123456 1 2 \t1.00\t2.00\t3.00\t4.00\t5.00\t6.000\t7.000\t8.000\r\n");
+    }(), "Format method failed");
+
     // multiple arguments
     static_assert([]{
         Array<char, 24> buffer;
