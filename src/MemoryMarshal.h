68296976c719b9e237276a1a3e286b438ecab8b1
Jeff Haynes <feedyurhed@gmail.com>
2026-05-10 14:26:38 -0400

    Add overloaded AsSpan and AsConstSpan methods to MemoryMarshal class

--- src/MemoryMarshal.h
+++ src/MemoryMarshal.h
@@ -41,6 +41,42 @@ concept Unsafe = !Safe<T>;
 class MemoryMarshal
 {
 public:
+    template<typename T>
+    static constexpr Span<T> AsSpan(T& value)
+    {
+        return Span<T>(&value, 1);
+    }
+
+    template<typename T>
+    static constexpr Span<const T> AsConstSpan(const T& value)
+    {
+        return Span<const T>(&value, 1);
+    }
+
+    template<typename T, uint32_t Length>
+    static constexpr Span<T> AsSpan(T (&value)[Length])
+    {
+        return Span<T>(value);
+    }
+
+    template<typename T, uint32_t Length>
+    static constexpr Span<const T> AsConstSpan(const T (&value)[Length])
+    {
+        return Span<const T>(value);
+    }
+
+    template<typename T>
+    static constexpr Span<T> AsSpan(T* data, uint32_t length)
+    {
+        return Span<T>(data, length);
+    }
+
+    template<typename T>
+    static constexpr Span<const T> AsConstSpan(const T* data, uint32_t length)
+    {
+        return Span<const T>(data, length);
+    }
+
     template<Safe T>
     static Span<uint8_t> AsBytes(T& value)
     {
